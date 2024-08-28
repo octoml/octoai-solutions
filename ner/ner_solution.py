32635c4198e787529d46df08ca8c179444f0fc61
Thierry Moreau <tmoreau@octoml.ai>
2024-08-28 15:53:48 -0700

    adding the user configurable schema in YAML format

--- ner/ner_solution.py
+++ ner/ner_solution.py
@@ -2,16 +2,56 @@ import json
 import os
 import pandas as pd
 import streamlit as st
+import yaml
 
+from code_editor import code_editor
 from llama_parse import LlamaParse
 from openai import OpenAI
 from pathlib import Path
 
+def convert_to_json_schema(yaml_str):
+    # Process yaml_dict
+    yaml_dict = yaml.load(yaml_str, Loader=yaml.SafeLoader)
+
+    # Prepare the return string
+    ret_str = "{\"properties\": {"
+    for name, value in yaml_dict.items():
+        description = ""
+        if "desc" in value:
+            description = value["desc"]
+        ret_str += "\"{}\": {{".format(name)
+        ret_str += "\"description\": \"{}\", ".format(description)
+        ret_str += "\"title\": \"{}\", ".format(name.replace("_", " ").title())
+        ret_str += "\"type\": \"string\"}, "
+    ret_str = ret_str[:-2]
+    ret_str += "}, \"required\": ["
+    for name, value in yaml_dict.items():
+        ret_str += "\"{}\", ".format(name)
+    ret_str = ret_str[:-2]
+    ret_str += "], \"title\": \"JSONObject\", \"type\": \"object\"}"
+
+    return ret_str
 
 st.set_page_config(layout="wide", page_title="NER Solution")
 st.write("## NER Solution")
 
 octoai_api_key = st.sidebar.text_input("OctoAI API Token [(get yours here)](https://octoai.cloud/n)", type="password")
+
+# Default schema - in a YAML file format
+
+yaml_format = """
+# Describe the fields of information in YAML format
+doc_title:
+    desc: title of the document
+authors:
+    desc: list of authors
+author_emails:
+    desc: list of author emails
+executive_summary:
+    desc: executive summary of the document
+"""
+
+response_dict = code_editor(code=yaml_format, lang="yaml")
 
 pdf_file = st.file_uploader("Upload your PDF file here", type=".pdf")
 
@@ -22,7 +62,8 @@ parser = LlamaParse(
     result_type="markdown"
 )
 
-if pdf_file:
+if pdf_file and octoai_api_key:
+    # Preprocess PDF
     with st.status("Processing the PDFs into Markdown form..."):
         # Store to disk
         # FIXME - tmoreau: let's not do this in the final version
@@ -36,6 +77,9 @@ if pdf_file:
             doc_str += document.text
             doc_str += "\n"
 
+    # Prepare the JSON schema
+    json_schema = convert_to_json_schema(response_dict['text'])
+
     # Let's do some LLM magic here
     with st.status("Converting to JSON form..."):
         client = OpenAI(
@@ -43,15 +87,17 @@ if pdf_file:
             api_key=octoai_api_key,
         )
         system_prompt = """
-You are an expert LLM that processes large markdown files and returns the detailed information in a JSON structured format.
-It's important to retain as much information from the input text in markdown in the JSON object - therefore JSON object should contain a LOT of information.
-ONLY RETURN THE JSON SCHEMA, DON'T SAY ANYTHING ELSE, THIS IS CRUCIAL.
+You are an expert LLM that processes large files and extracts entities according to the provided JSON schema:
+
+{}
+
+ONLY RETURN THE JSON OBJECT, DON'T SAY ANYTHING ELSE, THIS IS CRUCIAL.
 """
 
         data = {
             "model": "meta-llama-3.1-70b-instruct",
             "messages": [
-                {"role": "system", "content": system_prompt},
+                {"role": "system", "content": system_prompt.format(json_schema)},
                 {"role": "user", "content": doc_str}
             ],
             "temperature": 0,
@@ -63,7 +109,6 @@ ONLY RETURN THE JSON SCHEMA, DON'T SAY ANYTHING ELSE, THIS IS CRUCIAL.
         json_output = json_output.replace("```json", "")
         json_output = json_output.replace("```", "")
         json_output = json.loads(json_output)
-        print(json_output)
         data_frame = pd.json_normalize(json_output)
 
     st.dataframe(data_frame)
